9a1b6f9911f270b5f172f9ff5931fec96e3b1eb5
Curt Welch <curt@kcwc.com>
2017-07-01 00:32:23 -0700

    class code and getT working

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -46,8 +46,7 @@ def rot_z(q):
 class Kuka_KR210:
     def __init__(self):
         self.wrist_length = 0.303
-        # self.do_forward()
-        if 1:
+        if 0:
             self.do_test()
 
     def do_test(self):
@@ -75,108 +74,19 @@ class Kuka_KR210:
             roll, pitch, yaw = 0.0, 0.0, 0.0
             theta1, theta2, theta3, theta4, theta5, theta6 = self.do_IK(px, py, pz, roll, pitch, yaw, debug=True)
 
+        print "Repeat last test without debug"
+        theta1, theta2, theta3, theta4, theta5, theta6 = self.do_IK(px, py, pz, roll, pitch, yaw, debug=False)
+        print "angles are", theta1, theta2, theta3, theta4, theta5, theta6
+
         sys.exit(0)
 
 
-    def do_forward(self):
-        global q1, q2, q3, q4, q5, q6, q7 
-        q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
-        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
-        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
-        alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
-
-        print "Calculate Forward Kinematic Matrix"
-                
-        s = {alpha0: 0,      a0: 0,
-            alpha1: -pi/2,  a1: 0.35,   d1: 0.75,
-            alpha2: 0,      a2: 1.25,   d2: 0,
-            alpha3: -pi/2,  a3: -0.054, d3: 0,
-            alpha4: pi/2,   a4: 0,      d4: 1.5,
-            alpha5: -pi/2,  a5: 0,      d5: 0,
-            alpha6: 0,      a6: 0,      d6: 0,
-                                        d7: 0.303,  q7: 0}
-        # s.update({q1: 0, q2: -pi/2, q3: 0, q4: 0, q5: 0, q6: 0})
-        # s.update({q2: -pi/2, q3: 0, q4: 0, q5: 0, q6: 0})
-        # s.update({q3: 0, q4: 0, q5: 0, q6: 0})
-        # s.update({q4: 0, q5: 0, q6: 0})
-
-        T0_1 = Matrix([ [cos(q1),              -sin(q1),            0,              a0],
-                        [sin(q1)*cos(alpha0),  cos(q1)*cos(alpha0), -sin(alpha0),   -sin(alpha0)*d1],
-                        [sin(q1)*sin(alpha0),  cos(q1)*sin(alpha0), cos(alpha0),   cos(alpha0)*d1],
-                        [0,                     0,                  0,              1]])
-        T0_1 = T0_1.subs(s)
-
-        T1_2 = Matrix([ [cos(q2),              -sin(q2),            0,              a1],
-                        [sin(q2)*cos(alpha1),  cos(q2)*cos(alpha1), -sin(alpha1),   -sin(alpha1)*d2],
-                        [sin(q2)*sin(alpha1),  cos(q2)*sin(alpha1), cos(alpha1),   cos(alpha1)*d2],
-                        [0,                     0,                  0,              1]])
-        T1_2 = T1_2.subs(s)
-
-        T2_3 = Matrix([ [cos(q3),              -sin(q3),            0,              a2],
-                        [sin(q3)*cos(alpha2),  cos(q3)*cos(alpha2), -sin(alpha2),   -sin(alpha2)*d3],
-                        [sin(q3)*sin(alpha2),  cos(q3)*sin(alpha2), cos(alpha2),   cos(alpha2)*d3],
-                        [0,                     0,                  0,              1]])
-        T2_3 = T2_3.subs(s)
-
-        T3_4 = Matrix([ [cos(q4),              -sin(q4),            0,              a3],
-                        [sin(q4)*cos(alpha3),  cos(q4)*cos(alpha3), -sin(alpha3),   -sin(alpha3)*d4],
-                        [sin(q4)*sin(alpha3),  cos(q4)*sin(alpha3), cos(alpha3),   cos(alpha3)*d4],
-                        [0,                     0,                  0,              1]])
-        T3_4 = T3_4.subs(s)
-
-        T4_5 = Matrix([ [cos(q5),              -sin(q5),            0,              a4],
-                        [sin(q5)*cos(alpha4),  cos(q5)*cos(alpha4), -sin(alpha4),   -sin(alpha4)*d5],
-                        [sin(q5)*sin(alpha4),  cos(q5)*sin(alpha4), cos(alpha4),   cos(alpha4)*d5],
-                        [0,                     0,                  0,              1]])
-        T4_5 = T4_5.subs(s)
-
-        T5_6 = Matrix([ [cos(q6),              -sin(q6),            0,              a5],
-                        [sin(q6)*cos(alpha5),  cos(q6)*cos(alpha5), -sin(alpha5),   -sin(alpha5)*d6],
-                        [sin(q6)*sin(alpha5),  cos(q6)*sin(alpha5), cos(alpha5),   cos(alpha5)*d6],
-                        [0,                     0,                  0,              1]])
-        T5_6 = T5_6.subs(s)
-
-        T6_G = Matrix([ [cos(q7),              -sin(q7),            0,              a6],
-                        [sin(q7)*cos(alpha6),  cos(q7)*cos(alpha6), -sin(alpha6),   -sin(alpha6)*d7],
-                        [sin(q7)*sin(alpha6),  cos(q7)*sin(alpha6), cos(alpha6),   cos(alpha6)*d7],
-                        [0,                     0,                  0,              1]])
-        T6_G = T6_G.subs(s)
-
-        global T0_2, T0_3, T0_6, T0_G
-        T0_2 = (T0_1 * T1_2)
-        T0_3 = (T0_2 * T2_3)
-        T0_4 = (T0_3 * T3_4)
-        T0_5 = (T0_4 * T4_5)
-        T0_6 = (T0_5 * T5_6)
-        T0_G = (T0_6 * T6_G)
-
-        # Rotations to transform(/fix) final DH axes of gripper to world axes
-        # Rotate about z by 180 deg
-        R_z = Matrix([  [cos(pi),    -sin(pi),    0,      0],
-                        [sin(pi),    cos(pi),     0,      0],
-                        [0,             0,              1,      0],
-                        [0,             0,              0,      1]])
-
-        # Rotate about y by -90 deg
-        R_y = Matrix([  [cos(-pi/2),     0,      sin(-pi/2),  0],
-                        [0,                 1,      0,              0],
-                        [-sin(-pi/2),    0,      cos(-pi/2),  0],
-                        [0,                 0,      0,              1]])
-
-        R_corr = (R_z * R_y)
-
-        global T_total
-        T_total = (T0_G * R_corr)
-
-        self.wrist_length = s[d7]
-
-
-    def get_R(n, t1=None, t2=None, t3=None, t4=None, t5=None, t6=None):
+    def getR(self, n, t1=None, t2=None, t3=None, t4=None, t5=None, t6=None):
         # Get rotation matrix for different arm frames, n=1 to 8
-        r = self.get_T(n, t1=t1, t2=td2, t3=t3, t4=t4, t5=t5, t6=t6)
+        r = self.getT(n, t1=t1, t2=td2, t3=t3, t4=t4, t5=t5, t6=t6)
         return r[:3,:3]
 
-    def get_T(n=1, t1=None, t2=None, t3=None, t4=None, t5=None, t6=None):
+    def getT(self, n, t1=None, t2=None, t3=None, t4=None, t5=None, t6=None):
 
         # Return Homogeneous transformation matrix for arm frames
         # n controls which frame matrix is returned (1 to 8)
@@ -186,7 +96,7 @@ class Kuka_KR210:
         # T0_world is same as T0_G except axis rotated to world frame of z up, x forward
 
         if n == 0:
-            return identity(4)
+            return eye(4)
 
         q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
         d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
@@ -226,64 +136,63 @@ class Kuka_KR210:
                         [sin(q1)*cos(alpha0),  cos(q1)*cos(alpha0), -sin(alpha0),   -sin(alpha0)*d1],
                         [sin(q1)*sin(alpha0),  cos(q1)*sin(alpha0), cos(alpha0),   cos(alpha0)*d1],
                         [0,                     0,                  0,              1]])
-        T0_1 = T0_1.subs(s)
+        T0_1 = T0_1.subs(s).evalf()
         if n == 1:
-            return TO_1
+            return T0_1
 
         T1_2 = Matrix([ [cos(q2),              -sin(q2),            0,              a1],
                         [sin(q2)*cos(alpha1),  cos(q2)*cos(alpha1), -sin(alpha1),   -sin(alpha1)*d2],
                         [sin(q2)*sin(alpha1),  cos(q2)*sin(alpha1), cos(alpha1),   cos(alpha1)*d2],
                         [0,                     0,                  0,              1]])
         T1_2 = T1_2.subs(s)
+        T0_2 = (T0_1 * T1_2).evalf()
         if n == 2:
-            return TO_2
+            return T0_2
 
         T2_3 = Matrix([ [cos(q3),              -sin(q3),            0,              a2],
                         [sin(q3)*cos(alpha2),  cos(q3)*cos(alpha2), -sin(alpha2),   -sin(alpha2)*d3],
                         [sin(q3)*sin(alpha2),  cos(q3)*sin(alpha2), cos(alpha2),   cos(alpha2)*d3],
                         [0,                     0,                  0,              1]])
         T2_3 = T2_3.subs(s)
+        T0_3 = (T0_2 * T2_3).evalf()
         if n == 3:
-            return TO_3
+            return T0_3
 
         T3_4 = Matrix([ [cos(q4),              -sin(q4),            0,              a3],
                         [sin(q4)*cos(alpha3),  cos(q4)*cos(alpha3), -sin(alpha3),   -sin(alpha3)*d4],
                         [sin(q4)*sin(alpha3),  cos(q4)*sin(alpha3), cos(alpha3),   cos(alpha3)*d4],
                         [0,                     0,                  0,              1]])
         T3_4 = T3_4.subs(s)
+        T0_4 = (T0_3 * T3_4)
         if n == 4:
-            return TO_4
+            return T0_4
 
         T4_5 = Matrix([ [cos(q5),              -sin(q5),            0,              a4],
                         [sin(q5)*cos(alpha4),  cos(q5)*cos(alpha4), -sin(alpha4),   -sin(alpha4)*d5],
                         [sin(q5)*sin(alpha4),  cos(q5)*sin(alpha4), cos(alpha4),   cos(alpha4)*d5],
                         [0,                     0,                  0,              1]])
         T4_5 = T4_5.subs(s)
+        T0_5 = (T0_4 * T4_5)
         if n == 5:
-            return TO_5
+            return T0_5
 
         T5_6 = Matrix([ [cos(q6),              -sin(q6),            0,              a5],
                         [sin(q6)*cos(alpha5),  cos(q6)*cos(alpha5), -sin(alpha5),   -sin(alpha5)*d6],
                         [sin(q6)*sin(alpha5),  cos(q6)*sin(alpha5), cos(alpha5),   cos(alpha5)*d6],
                         [0,                     0,                  0,              1]])
         T5_6 = T5_6.subs(s)
+        T0_6 = (T0_5 * T5_6)
         if n == 6:
-            return TO_6
+            return T0_6
 
         T6_G = Matrix([ [cos(q7),              -sin(q7),            0,              a6],
                         [sin(q7)*cos(alpha6),  cos(q7)*cos(alpha6), -sin(alpha6),   -sin(alpha6)*d7],
                         [sin(q7)*sin(alpha6),  cos(q7)*sin(alpha6), cos(alpha6),   cos(alpha6)*d7],
                         [0,                     0,                  0,              1]])
         T6_G = T6_G.subs(s)
-        if n == 7:
-            return TO_G
-
-        T0_2 = (T0_1 * T1_2)
-        T0_3 = (T0_2 * T2_3)
-        T0_4 = (T0_3 * T3_4)
-        T0_5 = (T0_4 * T4_5)
-        T0_6 = (T0_5 * T5_6)
         T0_G = (T0_6 * T6_G)
+        if n == 7:
+            return T0_G
 
         # Rotations to transform(/fix) final DH axes of gripper to world axes
         # Rotate about z by 180 deg
@@ -302,10 +211,7 @@ class Kuka_KR210:
 
         T_total = (T0_G * R_corr)
 
-        if n == 8:
-            return T_total
-
-        return None # Error really
+        return T_total
 
     def do_IK(self, px, py, pz, roll, pitch, yaw, debug=False):
 
@@ -369,14 +275,15 @@ class Kuka_KR210:
 
         theta1 = atan2(wc[1], wc[0]).evalf() ## The simple one
 
-        # print "theta1 is", r_to_d(theta1), "degres"
-
-        global q1, q2, q3, q4, q5, q6, q7 
-        global T0_2, T0_3, T0_6, T0_G
+        # print "theta1 is", self.r_to_d(theta1), "degres"
 
         o1 = Matrix([0.0, 0.0, 0.75, 1.0])
-        t2 = T0_2.evalf(subs={q1: theta1, q2: 0})
-        o2 = (t2*Matrix([0,0,0,1])).evalf()
+
+        T2 = self.getT(2, t1=theta1, t2=0.0)
+        o2 = (T2*Matrix([0,0,0,1])).evalf()
+
+        # print "T2"
+        # pprint(T2)
 
         # print "o2:"
         # pprint(o2)
@@ -406,7 +313,7 @@ class Kuka_KR210:
         o2a = acos((l34**2 - l23**2 - l24**2) / (-2*l23*l24))
         o4a = (pi - o3a - o2a).evalf()
 
-        # print "triangles are", r_to_d(o3a), r_to_d(o2a), r_to_d(o4a)
+        # print "triangles are", self.r_to_d(o3a), self.r_to_d(o2a), self.r_to_d(o4a)
 
         # Calculate angle from horozontal of imiginary third side which is
         # running from joint 2, to the wrist center.
@@ -425,15 +332,15 @@ class Kuka_KR210:
 
         theta2 = (pi/2 - (o2o4horz + o2a)).evalf() # straight up is zero for robot
 
-        # print "Theta2 is", r_to_d(theta2), "which is pi/2 - o2a - o2o4horz", r_to_d(o2a), r_to_d(o2o4horz)
+        # print "Theta2 is", self.r_to_d(theta2), "which is pi/2 - o2a - o2o4horz", self.r_to_d(o2a), self.r_to_d(o2o4horz)
 
         # Now that we know theta2 (and q2) use FK to compute location of o3
 
-        t2 = T0_2.evalf(subs={q1: theta1, q2: theta2-pi/2})
+        T2 = self.getT(2, t1=theta1, t2=theta2-pi/2)
 
-        # print "q2 is", r_to_d(theta2-pi/2)
+        # print "q2 is", self.r_to_d(theta2-pi/2)
 
-        o3 = t2*Matrix([1.25, 0.0, 0.0, 1])
+        o3 = T2*Matrix([1.25, 0.0, 0.0, 1])
 
         # print "o3 is:"
         # pprint(o3)
@@ -450,12 +357,12 @@ class Kuka_KR210:
 
         theta3 = (pi/2 - (o3a + o3o4offset)).evalf()
         
-        # print "o3o4offset angle is", r_to_d(o3o4offset), "theta3 is", r_to_d(theta3)
+        # print "o3o4offset angle is", self.r_to_d(o3o4offset), "theta3 is", self.r_to_d(theta3)
 
         # Use FK to find o4 location
 
-        t3 = T0_3.evalf(subs={q1: theta1, q2: theta2-pi/2, q3: theta3})
-        o4 = (t3*Matrix([-0.054, 1.5, 0.0, 1])).evalf()
+        T3 = self.getT(3, t1=theta1, t2=theta2-pi/2, t3=theta3)
+        o4 = (T3*Matrix([-0.054, 1.5, 0.0, 1])).evalf()
 
         # print "o4 is", o4
         # print "wc (should be same as 04)", wc
@@ -474,29 +381,29 @@ class Kuka_KR210:
 
             # What's the distance from o3 to o4?  and o3 to wc?
             # o3o4distance = sqrt((o3[0]-o4[0])**2 + (o3[1]-o4[1])**2 + (o3[2]-o4[2])**2)
-            o3o4distance = distance(o3, o4)
+            o3o4distance = self.distance(o3, o4)
             print "distance from o3 to o4 is", o3o4distance, "should be same as l34:", l34, "which should be a little bit larget than 1.5"
             #o3wcdistance = sqrt((o3[0]-wc[0])**2 + (o3[1]-wc[1])**2 + (o3[2]-wc[2])**2)
-            o3wcdistance = distance(o3, wc)
+            o3wcdistance = self.distance(o3, wc)
             print "distance from o3 to wc is", o3wcdistance, "should be same as l34:", l34, "which should be a little bit larget than 1.5"
 
-            print "distance from o2 to o3", distance(o2, o3)
-            print "distance from o2 to wc", distance(o2, wc)
-            print "distance from o2 to o4", distance(o2, o4)
-            print "distance from 000 to o2", distance(Matrix([0,0,0]), o2)
+            print "distance from o2 to o3", self.distance(o2, o3)
+            print "distance from o2 to wc", self.distance(o2, wc)
+            print "distance from o2 to o4", self.distance(o2, o4)
+            print "distance from 000 to o2", self.distance(Matrix([0,0,0]), o2)
             print "distance from .35/.75", sqrt(.35**2 + .75**2)
 
         if 0:
             # More debug and test
             # OK, calculate the triangle angle and sides again using o2, o3, and o4
-            l23 = distance(o2, o3)
-            l34 = distance(o3, o4)
-            l24 = distance(o2, o4)
+            l23 = self.distance(o2, o3)
+            l34 = self.distance(o3, o4)
+            l24 = self.distance(o2, o4)
             print "NEW tri sides (l34, l23, l24)", l34, l23, l24
             o3a = acos((l24**2 - l23**2 - l34**2) / (-2*l23*l34))
             o2a = acos((l34**2 - l23**2 - l24**2) / (-2*l23*l24))
             o4a = (pi - o3a - o2a).evalf()
-            print "NEW triangles are", r_to_d(o3a), r_to_d(o2a), r_to_d(o4a)
+            print "NEW triangles are", self.r_to_d(o3a), self.r_to_d(o2a), self.r_to_d(o4a)
 
         #############################################
         # Now find q4 q5 and q6 for the end effector
@@ -512,7 +419,7 @@ class Kuka_KR210:
         # the rotation of the first three joints by using
         # the FK roation for R0_3.
 
-        R3_w = t3.transpose()[:3,:3] * R_rpy
+        R3_w = T3.transpose()[:3,:3] * R_rpy
 
         # R3_w now translates from link3 local frame (z along gripper,  x up) to
         # the gripper frame, expressed in world frame axis convention of z up
@@ -547,10 +454,9 @@ class Kuka_KR210:
         theta5 = b
         theta6 = g
 
-        if 1:
-            global T_total
-            tt = T0_G.evalf(subs={q1: theta1, q2: theta2-pi/2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
-            ttr = T_total.evalf(subs={q1: theta1, q2: theta2-pi/2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
+        if debug:
+            tt = self.getT(7, t1=theta1, t2=theta2-pi/2, t3=theta3, t4=theta4, t5=theta5, t6=theta6)
+            ttr = self.getT(8, t1=theta1, t2=theta2-pi/2, t3=theta3, t4=theta4, t5=theta5, t6=theta6)
 
             tt_end = tt * Matrix([0,0,0,1])
             tt_wc = ttr * Matrix([-self.wrist_length,0,0,1])
@@ -559,8 +465,8 @@ class Kuka_KR210:
                 print "tt is", np.array(tt).astype(np.float64)
                 print "tt_end is", tt_end
                 print "tt_wc is", tt_wc
-                print "distance from tt_end to wc is", distance(wc, tt_end)
-                print "distance from tt_end to pxyz is", distance(tt_end, Matrix([px, py, pz]))
+                print "distance from tt_end to wc is", self.distance(wc, tt_end)
+                print "distance from tt_end to pxyz is", self.distance(tt_end, Matrix([px, py, pz]))
 
             Rtt = np.array(ttr[:3,:3]).astype(np.float64)
 
@@ -569,35 +475,32 @@ class Kuka_KR210:
             # r,p,y = tf.transformations.euler_from_matrix(Rtt)
             # print "final rpy is", r, p, y
 
-        #########################################
-        # Verify we got a valid answer!
-        #########################################
+            #########################################
+            # Verify we got a valid answer!
+            #########################################
 
-        end_xyz = np.array(tt_end[:3,:]).astype(np.float64).flatten()
-        if not np.allclose(end_xyz, [px, py, pz]):
-            print "ERROR -- final gripper location fails to match target"
-            print "  target px, py, pz", px, py, pz
-            print "  result px, py, pz", end_xyz[0], end_xyz[1], end_xyz[2]
-            debug = True
+            end_xyz = np.array(tt_end[:3,:]).astype(np.float64).flatten()
+            if not np.allclose(end_xyz, [px, py, pz]):
+                print "ERROR -- final gripper location fails to match target"
+                print "  target px, py, pz", px, py, pz
+                print "  result px, py, pz", end_xyz[0], end_xyz[1], end_xyz[2]
 
-        r,p,y = tf.transformations.euler_from_matrix(Rtt)
-        if not np.allclose([roll, pitch, yaw], [r, p, y]):
-            print "ERROR -- final gripper pose fails to match target"
-            print "  target roll pitch yaw:", roll, pitch, yaw
-            print "  result roll pitch yaw:", r, p, y
-            debug = True
+            r,p,y = tf.transformations.euler_from_matrix(Rtt)
+            if not np.allclose([roll, pitch, yaw], [r, p, y]):
+                print "ERROR -- final gripper pose fails to match target"
+                print "  target roll pitch yaw:", roll, pitch, yaw
+                print "  result roll pitch yaw:", r, p, y
         
-        if debug:
             print "angles are:", theta1, theta2, theta3, theta4, theta5, theta6
 
         return theta1, theta2, theta3, theta4, theta5, theta6
 
     # Distance between two 3D points
-    def distance(p1, p2):
+    def distance(self, p1, p2):
         return (sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)).evalf()
 
     # Radians to Degrees for debuging
-    def r_to_d(x):
+    def r_to_d(self, x):
         return (x * 180.0 / pi).evalf()
 
 Robot = None
