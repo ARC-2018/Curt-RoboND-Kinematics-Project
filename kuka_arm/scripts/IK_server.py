7b6b94d543a00ee360a579bcf5b98ab95488d2e5
Curt Welch <curt@kcwc.com>
2017-06-30 23:42:03 -0700

    checkpoint

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -9,6 +9,9 @@
 
 # Author: Harsh Pandya
 
+# Student: Curt Welch <curt@kcwc.com>
+# July 1, 2017
+
 # import modules
 import rospy
 import tf
@@ -43,8 +46,8 @@ def rot_z(q):
 class Kuka_KR210:
     def __init__(self):
         self.wrist_length = 0.303
-        self.do_forward()
-        if 0:
+        # self.do_forward()
+        if 1:
             self.do_test()
 
     def do_test(self):
@@ -73,7 +76,6 @@ class Kuka_KR210:
             theta1, theta2, theta3, theta4, theta5, theta6 = self.do_IK(px, py, pz, roll, pitch, yaw, debug=True)
 
         sys.exit(0)
-
 
 
     def do_forward(self):
@@ -161,12 +163,149 @@ class Kuka_KR210:
                         [-sin(-pi/2),    0,      cos(-pi/2),  0],
                         [0,                 0,      0,              1]])
 
-        R_corr = simplify(R_z * R_y)
+        R_corr = (R_z * R_y)
 
         global T_total
-        T_total = simplify(T0_G * R_corr)
+        T_total = (T0_G * R_corr)
 
         self.wrist_length = s[d7]
+
+
+    def get_R(n, t1=None, t2=None, t3=None, t4=None, t5=None, t6=None):
+        # Get rotation matrix for different arm frames, n=1 to 8
+        r = self.get_T(n, t1=t1, t2=td2, t3=t3, t4=t4, t5=t5, t6=t6)
+        return r[:3,:3]
+
+    def get_T(n=1, t1=None, t2=None, t3=None, t4=None, t5=None, t6=None):
+
+        # Return Homogeneous transformation matrix for arm frames
+        # n controls which frame matrix is returned (1 to 8)
+        # rotation_only controls if only the rotation matrix is returned.
+        # n=1 returns T0_1, n=2 returns T0_2, ... n=7 returns T0_G, n=8 returns T0_world
+        # T0_G has griper hand at 0,0 with x up, z forward
+        # T0_world is same as T0_G except axis rotated to world frame of z up, x forward
+
+        if n == 0:
+            return identity(4)
+
+        q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+        print "Calculate Forward Kinematic Matrix for n=", n
+                
+        s = {alpha0: 0,      a0: 0,
+            alpha1: -pi/2,  a1: 0.35,   d1: 0.75,
+            alpha2: 0,      a2: 1.25,   d2: 0,
+            alpha3: -pi/2,  a3: -0.054, d3: 0,
+            alpha4: pi/2,   a4: 0,      d4: 1.5,
+            alpha5: -pi/2,  a5: 0,      d5: 0,
+            alpha6: 0,      a6: 0,      d6: 0,
+                                        d7: 0.303,  q7: 0}
+
+        # s.update({q1: 0, q2: -pi/2, q3: 0, q4: 0, q5: 0, q6: 0})
+        # s.update({q2: -pi/2, q3: 0, q4: 0, q5: 0, q6: 0})
+        # s.update({q3: 0, q4: 0, q5: 0, q6: 0})
+        # s.update({q4: 0, q5: 0, q6: 0})
+
+        if t1 is not None:
+            s.update({q1: t1})
+        if t2 is not None:
+            s.update({q2: t2})
+        if t3 is not None:
+            s.update({q3: t3})
+        if t4 is not None:
+            s.update({q4: t4})
+        if t5 is not None:
+            s.update({q5: t5})
+        if t6 is not None:
+            s.update({q6: t6})
+
+        T0_1 = Matrix([ [cos(q1),              -sin(q1),            0,              a0],
+                        [sin(q1)*cos(alpha0),  cos(q1)*cos(alpha0), -sin(alpha0),   -sin(alpha0)*d1],
+                        [sin(q1)*sin(alpha0),  cos(q1)*sin(alpha0), cos(alpha0),   cos(alpha0)*d1],
+                        [0,                     0,                  0,              1]])
+        T0_1 = T0_1.subs(s)
+        if n == 1:
+            return TO_1
+
+        T1_2 = Matrix([ [cos(q2),              -sin(q2),            0,              a1],
+                        [sin(q2)*cos(alpha1),  cos(q2)*cos(alpha1), -sin(alpha1),   -sin(alpha1)*d2],
+                        [sin(q2)*sin(alpha1),  cos(q2)*sin(alpha1), cos(alpha1),   cos(alpha1)*d2],
+                        [0,                     0,                  0,              1]])
+        T1_2 = T1_2.subs(s)
+        if n == 2:
+            return TO_2
+
+        T2_3 = Matrix([ [cos(q3),              -sin(q3),            0,              a2],
+                        [sin(q3)*cos(alpha2),  cos(q3)*cos(alpha2), -sin(alpha2),   -sin(alpha2)*d3],
+                        [sin(q3)*sin(alpha2),  cos(q3)*sin(alpha2), cos(alpha2),   cos(alpha2)*d3],
+                        [0,                     0,                  0,              1]])
+        T2_3 = T2_3.subs(s)
+        if n == 3:
+            return TO_3
+
+        T3_4 = Matrix([ [cos(q4),              -sin(q4),            0,              a3],
+                        [sin(q4)*cos(alpha3),  cos(q4)*cos(alpha3), -sin(alpha3),   -sin(alpha3)*d4],
+                        [sin(q4)*sin(alpha3),  cos(q4)*sin(alpha3), cos(alpha3),   cos(alpha3)*d4],
+                        [0,                     0,                  0,              1]])
+        T3_4 = T3_4.subs(s)
+        if n == 4:
+            return TO_4
+
+        T4_5 = Matrix([ [cos(q5),              -sin(q5),            0,              a4],
+                        [sin(q5)*cos(alpha4),  cos(q5)*cos(alpha4), -sin(alpha4),   -sin(alpha4)*d5],
+                        [sin(q5)*sin(alpha4),  cos(q5)*sin(alpha4), cos(alpha4),   cos(alpha4)*d5],
+                        [0,                     0,                  0,              1]])
+        T4_5 = T4_5.subs(s)
+        if n == 5:
+            return TO_5
+
+        T5_6 = Matrix([ [cos(q6),              -sin(q6),            0,              a5],
+                        [sin(q6)*cos(alpha5),  cos(q6)*cos(alpha5), -sin(alpha5),   -sin(alpha5)*d6],
+                        [sin(q6)*sin(alpha5),  cos(q6)*sin(alpha5), cos(alpha5),   cos(alpha5)*d6],
+                        [0,                     0,                  0,              1]])
+        T5_6 = T5_6.subs(s)
+        if n == 6:
+            return TO_6
+
+        T6_G = Matrix([ [cos(q7),              -sin(q7),            0,              a6],
+                        [sin(q7)*cos(alpha6),  cos(q7)*cos(alpha6), -sin(alpha6),   -sin(alpha6)*d7],
+                        [sin(q7)*sin(alpha6),  cos(q7)*sin(alpha6), cos(alpha6),   cos(alpha6)*d7],
+                        [0,                     0,                  0,              1]])
+        T6_G = T6_G.subs(s)
+        if n == 7:
+            return TO_G
+
+        T0_2 = (T0_1 * T1_2)
+        T0_3 = (T0_2 * T2_3)
+        T0_4 = (T0_3 * T3_4)
+        T0_5 = (T0_4 * T4_5)
+        T0_6 = (T0_5 * T5_6)
+        T0_G = (T0_6 * T6_G)
+
+        # Rotations to transform(/fix) final DH axes of gripper to world axes
+        # Rotate about z by 180 deg
+        R_z = Matrix([  [cos(pi),    -sin(pi),    0,      0],
+                        [sin(pi),    cos(pi),     0,      0],
+                        [0,             0,              1,      0],
+                        [0,             0,              0,      1]])
+
+        # Rotate about y by -90 deg
+        R_y = Matrix([  [cos(-pi/2),     0,      sin(-pi/2),  0],
+                        [0,                 1,      0,              0],
+                        [-sin(-pi/2),    0,      cos(-pi/2),  0],
+                        [0,                 0,      0,              1]])
+
+        R_corr = (R_z * R_y)
+
+        T_total = (T0_G * R_corr)
+
+        if n == 8:
+            return T_total
+
+        return None # Error really
 
     def do_IK(self, px, py, pz, roll, pitch, yaw, debug=False):
 
@@ -316,7 +455,7 @@ class Kuka_KR210:
         # Use FK to find o4 location
 
         t3 = T0_3.evalf(subs={q1: theta1, q2: theta2-pi/2, q3: theta3})
-        o4 = simplify(t3*Matrix([-0.054, 1.5, 0.0, 1])).evalf()
+        o4 = (t3*Matrix([-0.054, 1.5, 0.0, 1])).evalf()
 
         # print "o4 is", o4
         # print "wc (should be same as 04)", wc
@@ -356,7 +495,7 @@ class Kuka_KR210:
             print "NEW tri sides (l34, l23, l24)", l34, l23, l24
             o3a = acos((l24**2 - l23**2 - l34**2) / (-2*l23*l34))
             o2a = acos((l34**2 - l23**2 - l24**2) / (-2*l23*l24))
-            o4a = simplify(pi - o3a - o2a).evalf()
+            o4a = (pi - o3a - o2a).evalf()
             print "NEW triangles are", r_to_d(o3a), r_to_d(o2a), r_to_d(o4a)
 
         #############################################
